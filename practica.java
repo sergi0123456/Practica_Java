8e8c4031054dd447b588bb09bbfe2d468c7781c1
sergi0123456 <sergiterrones021@gmail.com>
2025-02-17 16:14:47 +0100

    Add files via upload

---
+++ practica.java
@@ -0,0 +1,34 @@
+package main;
+
+public class practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sumArray = {4,36,4,23,867};
+		int[] subArray = {785675, 56, 5675, 56544, 234};
+		int[] prodArray = {6, 8, 20, 23, 12};
+		
+		// User 1 fer la suma dels elements de sumArray
+		for(var i = 0; i <= sumArray.length; i++){
+        numero = sumArray[i];
+        suma += numero;
+    }
+		System.out.println("La suma del array es:"+sum)
+		
+		// User 2 fer la resta dels elements de subArray
+		for (int i = 1; i < subArray.length; i++) {
+    resta -= subArray[i];
+}
+
+System.out.println("La resta dels elements de subArray és: " + resta);
+		
+		
+		// User 3 fer la multiplicació dels elements de prodArray
+		for (int num : prodArray) {
+    multiplicacio *= num;
+}
+
+System.out.println("La multiplicació dels elements de prodArray és: " + multiplicació);
+	}
+
+}
